9431aeb958f9581aa0f5f644b82e5d1091fb1942
patman <27874300+patmanf@users.noreply.github.com>
2023-08-23 17:02:00 -0500

    owls can be caught with the bug net

---
+++ monsters/critter/owlcritter/pat_owlbugnet.lua
@@ -0,0 +1,12 @@
+local oldDamage = damage
+
+function damage(args)
+    if args.sourceKind == "bugnet" then
+        status.setResourcePercentage("health", 0)
+        return
+    end
+
+    if oldDamage then
+        oldDamage()
+    end
+end
